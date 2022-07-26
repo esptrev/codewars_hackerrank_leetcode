8a2b49b3cb711779b937186ad872f009dbfc4a29
Trevor Esparza <trevoresparza30@gmail.com>
2022-07-26 17:49:47 -0500

    leetcode problem

--- js/leet_code.js
+++ js/leet_code.js
@@ -110,3 +110,8 @@ var outlayer = [37,12,28,9,100,56,80,5,12];
 
 console.log(arrayRankTransform(outlayer));
 
+var removeDuplicates = function(nums) {
+    return [...new Set(nums)];
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
