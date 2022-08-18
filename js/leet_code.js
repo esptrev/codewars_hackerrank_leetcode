13c2e5dd96b870d73bea39726da0617d884330e5
Trevor Esparza <trevoresparza30@gmail.com>
2022-08-17 22:56:41 -0500

    triangle

--- js/leet_code.js
+++ js/leet_code.js
@@ -126,3 +126,29 @@ function checkScope(){
 }
 
 console.log(checkScope());
+
+///palindrome is a dataset that is the same forward and backwords
+var isPalindrome = (x) =>{
+    let reversed = parseInt(x.toString().split('').reverse().join(''));
+    if(x < 0){
+        return false
+    }else if(x === reversed){
+        return true;
+    }
+    return false;
+}
+
+console.log(isPalindrome(-11011));
+
+var minimumTotal = function(triangle) {
+    // Start from the 2nd to the bottom of the triangle
+    for (let i = triangle.length-2; i >= 0; i--)
+        // Add previous row cells to current cells and set the sum that is the smallest
+        for (let j = 0; j < triangle[i].length; j++)
+            triangle[i][j] += Math.min(triangle[i+1][j], triangle[i+1][j+1])
+    // The cell at the top of the triangle will be the smallest sum of the path that goes from the bottom to the top
+    return triangle[0][0]
+}
+
+
+console.log(minimumTotal([[-1],[2,3],[1,-1,-3]]));
