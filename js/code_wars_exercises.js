fe4a153e40686de6383af8f773a6720c52d649cb
Trevor Esparza <trevoresparza30@gmail.com>
2022-08-20 21:35:54 -0500

    merge two sorted array with placeholder

--- js/code_wars_exercises.js
+++ js/code_wars_exercises.js
@@ -632,3 +632,22 @@ var removeDuplicates = function(nums) {
     }
     console.log(nums);
 };
+
+var merge = function(nums1, m, nums2, n) {
+    let first = m - 1;
+    let second = n - 1;
+
+    for (var i = m + n - 1; i >= 0; i--) {
+        if (second < 0) {
+            break;
+        }
+
+        if (first >= 0 && nums1[first] > nums2[second]) {
+            nums1[i] = nums1[first];
+            first--;
+        } else {
+            nums1[i] = nums2[second];
+            second--;
+        }
+    }
+};
