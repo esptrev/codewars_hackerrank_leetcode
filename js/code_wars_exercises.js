799f6d6bb5ed0114205de82d4835c9676c142e17
Trevor Esparza <trevoresparza30@gmail.com>
2022-08-22 20:16:20 -0500

    multiply without parseint

--- js/code_wars_exercises.js
+++ js/code_wars_exercises.js
@@ -651,3 +651,17 @@ var merge = function(nums1, m, nums2, n) {
         }
     }
 };
+
+var multiply = function(num1, num2) {
+    const product = Array(num1.length+num2.length).fill(0);
+    for (let i = num1.length; i--; null) {
+        let carry = 0;
+        for (let j = num2.length; j--; null) {
+            product[1+i+j] += carry + num1[i]*num2[j];
+            carry = Math.floor(product[1+i+j] / 10);
+            product[1+i+j] = product[1+i+j] % 10;
+        }
+        product[i] += carry;
+    }
+    return product.join("").replace(/^0*(\d)/, "$1");
+};
