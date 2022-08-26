d19836f88b15831a612cf9f48d805a35f7f45602
Trevor Esparza <trevoresparza30@gmail.com>
2022-08-25 20:50:32 -0500

    leetcode 383

--- js/code_wars_exercises.js
+++ js/code_wars_exercises.js
@@ -743,3 +743,17 @@ var longestPalindrome = function(s) {
 
     return longestPal;
 };
+
+///leetcode 383
+const canConstruct = (ransomNote, magazine) => {
+    let dicObj = {};
+    for (let i = 0; i < magazine.length; ++i) {
+        if (dicObj[magazine[i]]) ++dicObj[magazine[i]];
+        else dicObj[magazine[i]] = 1;
+    }
+    for (let i = 0; i < ransomNote.length; ++i) {
+        if (dicObj[ransomNote[i]] === undefined || dicObj[ransomNote[i]] <= 0) return false;
+        --dicObj[ransomNote[i]];
+    }
+    return true;
+};
