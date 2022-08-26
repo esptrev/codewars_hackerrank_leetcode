c7925e552c9956974029269947dce9a692264597
Trevor Esparza <trevoresparza30@gmail.com>
2022-08-26 16:29:51 -0500

    quadratic

--- js/udemy_ds_algos.js
+++ js/udemy_ds_algos.js
@@ -1,14 +1,29 @@
 ///BIG O NOTATION///
-function addUpTo(n){
+function addUpToFirst(n){
     let total = 0;
     for (let i = 0; i <= n; i++) {
         total += i
     }
     return total;
 }
-console.log(addUpTo(6));
+// var time1= performance.now();
+// console.log(addUpToFirst(1000000000));
+// var time2=performance.now();
+// console.log(`Elapsed time: ${(time2 - time1) / 1000}`);
 
-function addUpTwo(n){
+function addUpToSecond(n){
     return n * (n+1) / 2;
 }
-console.log(addUpTwo(6));
+// var time3= performance.now();
+// console.log(addUpToSecond(1000000000));
+// var time4= performance.now();
+// console.log(`Elapsed time: ${(time4 - time3) / 1000}`);
+
+function printAllPairs(n){
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+             console.log(i + ',' + j);
+        }
+    }
+}
+printAllPairs(5);
