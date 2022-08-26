95dc262a48a8163269f84e4d8f289ee760bec696
Trevor Esparza <trevoresparza30@gmail.com>
2022-08-25 20:46:08 -0500

    leetcode 5

--- js/code_wars_exercises.js
+++ js/code_wars_exercises.js
@@ -702,3 +702,44 @@ var isPowerOfThree = function(n) {
         return isPowerOfThree(n/3);
     }
 };
+
+
+///Leetcode 5
+var longestPalindrome = function(s) {
+    let longestPal = '';
+
+    var getLongestPalindrome = function (leftPosition, rightPosition) {
+        // While there is space to expand, and the expanded strings match
+        while (
+            leftPosition >= 0 &&
+            rightPosition < string.length &&
+            string[leftPosition] === string[rightPosition]
+            ) {
+            // Expand in each direction.
+            leftPosition--;
+            rightPosition++;
+        }
+
+        // Store the longest palindrom (if it's the longest one found so far)
+        if (rightPosition - leftPosition > longestPal.length) {
+            longestPal = string.slice(leftPosition + 1, rightPosition);
+        }
+    };
+
+    // Loop through the letters
+    for (let i = 0; i < string.length; i++) {
+        // Find the longest odd palendrome
+        getLongestPalindrome(i, i + 1);
+
+        // Find the longest even palendrome
+        getLongestPalindrome(i, i);
+
+        // Check if a longer palindrome cannot be found
+        if ((string.length - i) * 2 < longestPal.length) {
+            // Break out to avoid unnecessary computation
+            break;
+        }
+    }
+
+    return longestPal;
+};
